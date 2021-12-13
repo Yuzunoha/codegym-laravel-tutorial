4c2e47d69e1f000f79c2db3d5f8e06a9113159c0
Yuzunoha <studysteadystandby@gmail.com>
2021-12-13 18:16:01 +0900

    detailカラムの位置を指定する

--- application/database/migrations/2021_12_13_181133_add_detail_to_tasks_table.php
+++ application/database/migrations/2021_12_13_181133_add_detail_to_tasks_table.php
@@ -14,7 +14,7 @@ class AddDetailToTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->string('detail');
+            $table->string('detail')->after('task_kind_id');
         });
     }
 
