89c44260c42415a23e4480b98ca7717427406b60
Yuzunoha <studysteadystandby@gmail.com>
2021-12-14 16:11:02 +0900

    自動整形

--- application/routes/web.php
+++ application/routes/web.php
@@ -23,7 +23,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
 Route::resource('projects', ProjectController::class)
     ->middleware(['auth']);
