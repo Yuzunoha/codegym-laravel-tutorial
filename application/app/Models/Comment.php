26a76484958f2cab6f6d1fac0940e038c2f43e13
Yuzunoha <studysteadystandby@gmail.com>
2021-12-14 16:43:30 +0900

    書いてみた

--- application/app/Models/Comment.php
+++ application/app/Models/Comment.php
@@ -4,8 +4,50 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Kyslik\ColumnSortable\Sortable;
 
 class Comment extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+    use Sortable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'task_id',
+        'user_id',
+        'text',
+    ];
+
+    /**
+     * ソート対象となる項目.
+     *
+     * @var array
+     */
+    public $sortable = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * コメントが属するタスクを取得.
+     */
+    public function task()
+    {
+        return $this->belongsTo(Task::class, 'task_id');
+    }
+
+    /**
+     * コメントを作成したユーザーを取得.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
