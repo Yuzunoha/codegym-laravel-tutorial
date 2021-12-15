002eaa478bd8156516e9971dd935b8efcffb49ac
Yuzunoha <studysteadystandby@gmail.com>
2021-12-15 19:14:41 +0900

    const modal_title = document.getElementById('id_modal_title');

--- application/resources/views/tasks/edit.blade.php
+++ application/resources/views/tasks/edit.blade.php
@@ -3,10 +3,9 @@
     const p = console.log;
     const deleteform = document.getElementById('id_deleteform');
     const modal_message = document.getElementById('id_modal_message');
+    const modal_title = document.getElementById('id_modal_title');
 
     function toggleModal() {
-        p('deleteform.action', deleteform.action);
-        p('modal_message.innerText', modal_message.innerText);
         const body = document.querySelector('body');
         const modal = document.querySelector('.modal');
         modal.classList.toggle('opacity-0');
@@ -200,7 +199,7 @@
 
                     <div class="modal-content py-4 text-left px-6">
                         <div class="flex justify-between items-center pb-3">
-                            <p class="text-2xl font-bold">{{ __('Are you sure you want to delete this task?') }}</p>
+                            <p id="id_modal_title" class="text-2xl font-bold">{{ __('Are you sure you want to delete this task?') }}</p>
                             <div class="modal-close cursor-pointer z-50">
                                 <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
                                     <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
