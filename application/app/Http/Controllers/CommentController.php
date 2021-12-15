7582d9536ccf42aba77735a0379ad5a30e088ea5
Yuzunoha <studysteadystandby@gmail.com>
2021-12-15 23:33:02 +0900

    use不要だった

--- application/app/Http/Controllers/CommentController.php
+++ application/app/Http/Controllers/CommentController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Comment;
 use App\Models\Project;
 use App\Models\Task;
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
